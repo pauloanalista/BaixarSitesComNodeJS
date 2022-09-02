0db208a90db147169a3ff561db4b9605cc7bde38
Paulo Rogerio Martins Marques <paulo.marques@uniodigital.com.br>
2022-09-02 13:32:32 -0300

    alterando site

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const {exec} = require('child_process')
-const websiteUrl = 'https://byrushan.com/projects/super-admin/app/2.2/';
+const websiteUrl = 'https://ilovecode.com.br';
 
   var directoryPath = Date.now()
   exec(`node-site-downloader download -s ${websiteUrl} -d ${websiteUrl} -o ${directoryPath} -v --include-images`,(err,stdout,stderr) =>{
