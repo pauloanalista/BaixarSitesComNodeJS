d7b2a45466e9c12c25269418a8ed0ec9fc2be887
Paulo Rogerio Martins Marques <paulo.marques@uniodigital.com.br>
2022-09-02 13:47:35 -0300

    adicionei site para baixar templates

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const {exec} = require('child_process')
-const websiteUrl = 'https://ilovecode.com.br';
+const websiteUrl = 'https://dashboardkit.io/bootstrap/navigation/index.html';
+//link com templates par abaixar
+//https://wrapbootstrap.com/category/templates/landing-page-templates
 
   var directoryPath = Date.now()
   exec(`node-site-downloader download -s ${websiteUrl} -d ${websiteUrl} -o ${directoryPath} -v --include-images`,(err,stdout,stderr) =>{
